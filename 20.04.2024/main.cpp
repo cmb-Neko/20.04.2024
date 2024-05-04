2f1064f5ddbe219f3d31902c5dcc4d8e8fb43c12
hiNekoUwU <h1t0nek0uwu@gmail.com>
2024-05-04 19:09:40 +0300

    hm_2

--- 20.04.2024/main.cpp
+++ 20.04.2024/main.cpp
@@ -1,17 +1,15 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main() {
-	double R1, R2, R3, R0;
-	cout << "Enter first value: ";
-	cin >> R1;
-	cout << "Enter second value: ";
-	cin >> R2;
-	cout << "Enter third value: ";
-	cin >> R3;
-	R0 = ((1 / R1) + (1 / R2) + (1 / R3));
-	R0 = (1 / R0);
-	cout << R0 << endl;
+	double L, R, S;
+	cout << "Enter length: ";
+	cin >> L;
+	R = (L / (2 * M_PI));
+	S = (M_PI * (R * 2));
+	cout << S << endl;
 }
